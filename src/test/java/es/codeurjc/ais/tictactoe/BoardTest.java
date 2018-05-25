7fcdf88540f20316ca3d1e9273bd3b6a1de41438
Marcos <marcosarquero@hotmail.com>
2018-05-25 14:36:51 +0200

    Tutoria 2

--- src/test/java/es/codeurjc/ais/tictactoe/BoardTest.java
+++ src/test/java/es/codeurjc/ais/tictactoe/BoardTest.java
@@ -4,7 +4,6 @@ package es.codeurjc.ais.tictactoe;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
